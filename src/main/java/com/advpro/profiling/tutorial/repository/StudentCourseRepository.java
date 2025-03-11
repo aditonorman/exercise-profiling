f1281208135395d9636d0c562d81461b70796a3d
aditonorman <muhammad.raditya33@ui.ac.id>
2025-03-12 02:11:04 +0700

    [Refactoring] - Optimizing method getAllStudentWithCourse

--- src/main/java/com/advpro/profiling/tutorial/repository/StudentCourseRepository.java
+++ src/main/java/com/advpro/profiling/tutorial/repository/StudentCourseRepository.java
@@ -1,16 +1,18 @@
 package com.advpro.profiling.tutorial.repository;
 
-import com.advpro.profiling.tutorial.model.Student;
 import com.advpro.profiling.tutorial.model.StudentCourse;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
 /**
- * @author muhammad.khadafi
+ * Optimized repository to fetch StudentCourse entities with their associated
+ * Student and Course entities in one query.
  */
 @Repository
 public interface StudentCourseRepository extends JpaRepository<StudentCourse, Long> {
-    List<StudentCourse> findByStudentId(Long studentId);
+
+    @Query("SELECT sc FROM StudentCourse sc JOIN FETCH sc.student JOIN FETCH sc.course")
+    List<StudentCourse> findAllWithStudentAndCourse();
 }
